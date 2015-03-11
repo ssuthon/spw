6072c24faec8a92f2b8bf7abf96604cc471b6804
Kerkieat <kerkieat@gmail.com>
2015-03-11 23:11:46 +0700

    edit step = 10
    edit step = 10

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -5,7 +5,7 @@ import java.awt.Graphics2D;
 
 public class SpaceShip extends Sprite{
 
-	int step = 8;
+	int step = 10;
 	
 	public SpaceShip(int x, int y, int width, int height) {
 		super(x, y, width, height);
